8372bdbf7ef3d826b715f9aa416196191329f904
Georgina Toye <48524807+GRToye@users.noreply.github.com>
2019-04-23 03:16:39 +0100

    Add files via upload

--- Parser.py
+++ Parser.py
@@ -1,64 +1,36 @@
 import re
-
 with open('genbank2.txt','rt') as myfile:
-    for line in myfile:   
+    for line in myfile: 
         acc_flag = re.compile('(ACCESSION)([\s]+)(([A-Z]+)([0-9]+))')
         acc_found = re.match(acc_flag,line)
         if acc_found != None:
             acc_code = acc_found.group(3)
             print(acc_code,end=',')
-print('\n')            
-with open('genbank2.txt','rt') as myfile:
-    for line in myfile:   
-        map_flag = re.compile('([\s]+)(/map=")(.+)(")')
-        map_found = re.match(map_flag,line)
-        if map_found != None:
-            chrom_loc = map_found.group(3)
-            print(chrom_loc,end=',')
-print('\n')
-with open('genbank2.txt','rt') as myfile:
-    for line in myfile:
-        gene_flag = re.compile('([\s]+)(/gene=")(.+)(")')
-        gene_found = re.match(gene_flag,line)
-        if gene_found != None:
-            gene_id = gene_found.group(3)
-            print(gene_id,end=',')
-print('\n')
-with open('genbank2.txt','rt') as myfile:
-    for line in myfile:
-        prot_flag = re.compile('([\s]+)(/product=")(.+)(")')
-        prot_found = re.match(prot_flag,line)
-        if prot_found != None:
-            prot_name = prot_found.group(3)
-            print(prot_name,end=',')
-print('\n')
-with open('genbank2.txt','rt') as myfile:
-    for line in myfile:
-        sour_flag = re.compile('(([\s]+)(source)([\s]+))((1..)([0-9]+))')
-        sour_found = re.match(sour_flag,line)
-        if sour_found != None:
-            gene_span = sour_found.group(5)
-            print(gene_span,end=',')
-print('\n')
-with open('genbank2.txt','rt') as myfile:
-    for line in myfile:
-        join_flag = re.compile('(([\s]+)(CDS)([\s]+))(join\()(([0-9])(.+))')
-        join_found = re.match(join_flag,line)
-        if join_found != None:
-            exon_map = join_found.group(6)
-            print(exon_map,end='')
-print('\n')
-with open('genbank2.txt','rt') as myfile:
-    for line in myfile:
-        star_flag = re.compile('([\s]+)(/codon_)((start=)([1-3]))')
-        star_found = re.match(star_flag,line)
-        if star_found != None:
-            start_cod = star_found.group(3)
-            print(start_cod,end=',')
-print('\n')
-gbfile = open('genbank2.txt','r')
-orig_flags = re.findall(r'(ORIGIN)',gbfile.read())
-for orig_flag in orig_flags:
-    gene_dna = orig_flag
-    print(gene_dna,end=',')
-print('\n')
+            
+f = open('genbank2.txt','rt')
+acc_found = re.findall(r'(ORIGIN)[\s*]+1[\s]([a-z\s]+)',f.read())
+if acc_found != None:
+    print(acc_found,end=',')
+
+f = open('genbank2.txt','rt')
+acc_found = re.findall(r'(ORIGIN)[\s*]+1[\s]([a-z]+[\s]+)',f.read())
+if acc_found != None:
+    print(acc_found,end=',')
+    
+f = open('genbank2.txt','rt')
+acc_found = re.findall(r'(ORIGIN)[\s*]+1[\s]([a-z]+)[\s]+([a-z]+)[\s]+([a-z]+)[\s]+',f.read())
+if acc_found != None:
+    print(acc_found,end=',')
+'''
+# Open file
+  f = open('test.txt', 'r')
+  # Feed the file text into findall(); it returns a list of all the found strings
+  strings = re.findall(r'some pattern', f.read())
+  k2 = re.findall(r"^\w", xx, re.MULTILINE)
+((?:[A-Z]+\n)+)
+with open('numbers.txt', 'r') as f:
+numbers = f.readlines()
+('ACCESSION', '   ', 'AB034984', 'AB', '034984')
+['t', 'a', ' ', 't', 't', 'c'], ['a', 't', 't', 't', 't', 't', 'g'],
+('(ORIGIN)([\s]+([A-Z0-9\s]+)(//))(ORIGIN)[\s]+(.+)\n[\s]*([0-9]
+'''
