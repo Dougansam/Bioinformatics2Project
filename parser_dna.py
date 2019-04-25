82774c0b838f5ea750d6dd9924ddb43cffb7adba
Georgina Toye <48524807+GRToye@users.noreply.github.com>
2019-04-25 14:31:12 +0100

    Add files via upload

--- parser_dna.py
+++ parser_dna.py
@@ -9,7 +9,7 @@ Description:
 ------------
 
 This program takes a text document containing Genbank records, and returns the
-complete DNA sequence, consecutively, from each record.
+complete DNA sequence taken from each record.
 
 --------------------------------------------------------------------------------
 '''
@@ -19,10 +19,13 @@ complete DNA sequence, consecutively, from each record.
 
 import re
 
+# detect & record gene sequences
 f = open('genbank2.txt','r')
 dna_found = re.findall(r'ORIGIN[\s*]+([a-z\s0-9]+)[\s*]//',f.read())
 if dna_found != None:
     dna_seqs = dna_found
+    
+    # trim, clean & print records
     dna_seqs = [re.sub(r'\s+','', dna_seq) for dna_seq in dna_seqs]
     dna_seqs = [re.sub(r'[0-9]+','', dna_seq) for dna_seq in dna_seqs]
     print(dna_seqs,end=',')
@@ -36,6 +39,6 @@ print('\n')
 
 None
 
---------------------------------------------------------------------------------'''
+--------------------------------------------------------------------------------
 
 '''  
