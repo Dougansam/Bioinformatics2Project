b78c9e7715c973a6af3eb922d05ec52052a043ff
Diego <48446612+copepodito@users.noreply.github.com>
2019-05-07 15:09:53 +0100

    Update config.py

--- cgi-biocomp2/config.py
+++ cgi-biocomp2/config.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 
-index_path = "http://student.cryst.bbk.ac.uk/~cd002/index.html"
-details_accession_path = "http://student.cryst.bbk.ac.uk/cgi-bin/cgiwrap/cd002/details.py?option=accession&query="
-favicon_path = "http://student.cryst.bbk.ac.uk/~cd002/favicon.ico"
-logo_path = "http://student.cryst.bbk.ac.uk/~cd002/logo.svg"
-microarray_fig_path = "http://student.cryst.bbk.ac.uk/~cd002/microarray.jpg"
+index_path = 'http://student.cryst.bbk.ac.uk/~cd002/index.html'
+details_accession_path = 'http://student.cryst.bbk.ac.uk/cgi-bin/cgiwrap/cd002/details.py?radio=accession&query='
+favicon_path = 'http://student.cryst.bbk.ac.uk/~cd002/favicon.ico'
+logo_path = 'http://student.cryst.bbk.ac.uk/~cd002/logo.svg'
+microarray_fig_path = 'http://student.cryst.bbk.ac.uk/~cd002/microarray.jpg'
 mysql_db_login = 'mysql -u tg001 -p"f29z70k#r" tg001'
-
-# Acces cgi-biocomp2, where config.py file is
-import sys
-sys.path.insert(0, "../cgi-biocomp2/")
-
-# Import the config.py itself
-import config
-
-# This is how you use the variable
-config.mysql_db_login
+css_path = 'http://student.cryst.bbk.ac.uk/~cd002/w3.css'
+simple_tab_path = 'http://student.cryst.bbk.ac.uk/~cd002/simple_tab.js'
+jquery_path = 'http://student.cryst.bbk.ac.uk/~cd002/jquery-3.4.1.min.js'
+re_highlight_plugin_path = 'http://student.cryst.bbk.ac.uk/~cd002/jquery.highlight-5.js'
+text_highlight_plugin_path = 'http://student.cryst.bbk.ac.uk/~cd002/textHighlight.js'
+app_path = 'http://student.cryst.bbk.ac.uk/~cd002/app.js'
