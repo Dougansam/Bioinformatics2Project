59da7884295f44266ee24bcecaf0ed3737aa32b0
Dougansam <48446341+Dougansam@users.noreply.github.com>
2019-05-07 17:24:11 +0100

    Add files via upload

--- cgi-biocomp2/bl/docs/BL test.py
+++ cgi-biocomp2/bl/docs/BL test.py
@@ -1,5 +1,6 @@
+import re
 import sys
-sys.path.insert(0, "../bl")
+sys.path.insert(0, "../")
 import blapi
 
 #test feature tests output of dummy data if the program is run directly.
@@ -8,11 +9,12 @@ import blapi
 if __name__ == "__main__":
     radio = "accession"
     typed = "test"
-    print (any_coding_re(radio ,typed))
-    print (coding_regions(radio, typed))
-    print (coding_seq(radio, typed))
-    print (codon_freq_gene(radio, typed))
-    print (protein_seq(radio, typed))
-    print (retrieve_all())
-    print (retrieve_basic(radio, typed))
-    print (show_known_re())
+    print (blapi.any_coding_re(radio ,typed))
+    print (blapi.coding_regions(radio, typed))
+    print (blapi.coding_seq(radio, typed))
+    print (blapi.codon_freq_gene(radio, typed))
+    print (blapi.protein_seq(radio, typed))
+    print (blapi.retrieve_all())
+    print (blapi.retrieve_basic(radio, typed))
+    print (blapi.show_known_re())
+    print (blapi.whole_chrom_codons())
