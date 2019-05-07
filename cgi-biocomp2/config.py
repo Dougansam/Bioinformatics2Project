b29bb4e84c0cc1b65bc2a2a0665a891f491db95f
Diego <48446612+copepodito@users.noreply.github.com>
2019-05-07 15:10:33 +0100

    Update config.py

--- cgi-biocomp2/config.py
+++ cgi-biocomp2/config.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
+mysql_db_login = 'mysql -u tg001 -p"f29z70k#r" tg001'
 index_path = 'http://student.cryst.bbk.ac.uk/~cd002/index.html'
 details_accession_path = 'http://student.cryst.bbk.ac.uk/cgi-bin/cgiwrap/cd002/details.py?radio=accession&query='
 favicon_path = 'http://student.cryst.bbk.ac.uk/~cd002/favicon.ico'
 logo_path = 'http://student.cryst.bbk.ac.uk/~cd002/logo.svg'
 microarray_fig_path = 'http://student.cryst.bbk.ac.uk/~cd002/microarray.jpg'
-mysql_db_login = 'mysql -u tg001 -p"f29z70k#r" tg001'
 css_path = 'http://student.cryst.bbk.ac.uk/~cd002/w3.css'
 simple_tab_path = 'http://student.cryst.bbk.ac.uk/~cd002/simple_tab.js'
 jquery_path = 'http://student.cryst.bbk.ac.uk/~cd002/jquery-3.4.1.min.js'
